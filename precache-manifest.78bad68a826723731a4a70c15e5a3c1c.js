4acdc1c01306e5333d613a333ff21e0f4194f38f
Olivia <mxue2@student.unimelb.edu.au>
2019-11-21 20:25:27 +1100

    Updates

---
+++ precache-manifest.78bad68a826723731a4a70c15e5a3c1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6546d1fe6d0a1b92e9d3670411e93404",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "464384cec45a4a8b64ea",
+    "url": "/Weather-App/static/css/main.0ac7925c.chunk.css"
+  },
+  {
+    "revision": "4950cc82aa434f5013fb",
+    "url": "/Weather-App/static/js/2.ddf9a4ef.chunk.js"
+  },
+  {
+    "revision": "464384cec45a4a8b64ea",
+    "url": "/Weather-App/static/js/main.c09dd839.chunk.js"
+  },
+  {
+    "revision": "d0b398f267de7108d8a0",
+    "url": "/Weather-App/static/js/runtime~main.9a3789a3.js"
+  },
+  {
+    "revision": "846f45c7e4426c36a1de1e5bc6630ae2",
+    "url": "/Weather-App/static/media/Clear.846f45c7.jpeg"
+  },
+  {
+    "revision": "9afbeb9e233e4f5b2981dc10664e0652",
+    "url": "/Weather-App/static/media/Clouds.9afbeb9e.jpeg"
+  },
+  {
+    "revision": "86726ded61097d17bf64fb7e7437d08e",
+    "url": "/Weather-App/static/media/Drizzle.86726ded.jpeg"
+  },
+  {
+    "revision": "9e2993e6c6eb8dfc6fdd4c00d5bd28f8",
+    "url": "/Weather-App/static/media/Fog.9e2993e6.jpeg"
+  },
+  {
+    "revision": "4d7bbd92efccedfbb02a986b128d06d2",
+    "url": "/Weather-App/static/media/Rain.4d7bbd92.jpeg"
+  },
+  {
+    "revision": "3fdfb20cf7fa88ba94038d83accb7009",
+    "url": "/Weather-App/static/media/Snow.3fdfb20c.jpeg"
+  },
+  {
+    "revision": "91ae2e4639229e5fb0a04f77dd4f37ae",
+    "url": "/Weather-App/static/media/Thunderstorm.91ae2e46.jpeg"
+  },
+  {
+    "revision": "a37f2fa962a63f1cf80622265d9be75c",
+    "url": "/Weather-App/static/media/default.a37f2fa9.jpg"
+  }
+]);
